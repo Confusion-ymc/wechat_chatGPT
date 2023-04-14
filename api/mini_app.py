04d2bd6ca9bc323a771ab08e0c6ede3db3cf071e
Confusion-ymc <32094374+Confusion-ymc@users.noreply.github.com>
2023-04-14 14:27:50 +0800

    优化代码

--- api/mini_app.py
+++ api/mini_app.py
@@ -79,73 +79,62 @@ async def set_version(version: List[str], request: Request):
     return {'data': request.app.state.block_version}
 
 
-class WsUser:
+class NewUser:
     def __init__(self, user_id, bot_manager):
-        self.pending = False
-        self.bot_manager = bot_manager
+        self.run_task = False
         self.user_id = user_id
+        self.bot_manager = bot_manager
+        self.reply_cache = []
         self.ask_message = ''
-        self.reply_queue = []
 
-    def ask(self, ask_message):
-        if self.pending or self.reply_queue:
+    def ask_task(self, ask_message):
+        if self.run_task:
             logger.info('已存在运行中的任务, 不创建任务')
             return False
         else:
+            self.run_task = True
             self.ask_message = ask_message
-            self.pending = True
-            threading.Thread(target=self.stream_reply, args=(ask_message,)).start()
+            threading.Thread(target=self.chatgpt_stream_reply, args=(ask_message,)).start()
             return True
 
-    async def send_reply(self, ws: WebSocket):
+    async def reply_task(self, websocket: WebSocket):
         backup = []
-        if not self.pending and not self.reply_queue:
-            logger.error('回复线程退出')
-            return
-        try:
-            start_send = False
-            reply_message = ''
-            while True:
-                if self.reply_queue:
-                    send_data = self.reply_queue.pop(0)
-                    backup.append(send_data)
-                    if not start_send:
-                        logger.info(f'开始回复消息 [{self.user_id}] [{self.ask_message}]')
-                        start_send = True
-                    await ws.send_json(send_data)
-                    reply_message += send_data['data']
-                    if send_data['finish']:
-                        self.reply_queue = []
-                        break
-                else:
-                    await asyncio.sleep(0.1)
-            logger.info(f'全部消息成功发送 [{self.user_id}] [{self.ask_message}] [{reply_message}]')
+        while websocket.client_state.value == 1:
+            while self.reply_cache:
+                try:
+                    send_item = self.reply_cache.pop(0)
+                    backup.append(send_item)
+                    await websocket.send_json(send_item)
+                    if send_item['finish']:
+                        backup = []
+                except Exception as e:
+                    logger.error(f'[发送失败] {e}')
+                    break
+            await asyncio.sleep(0.1)
+        if len(backup):
+            logger.warning(
+                f"[回滚所有消息] [{self.ask_message}] [{''.join([item['data'] for item in backup])}]")
+            for i in range(len(backup)):
+                self.reply_cache.insert(0, backup[-(1 + i)])
+        logger.error('回复消息线程 退出')
 
-        except Exception as e:
-            logger.error(f'[发送失败] {e}')
-            if len(backup):
-                logger.warning(f"[回滚所有消息] [{self.ask_message}] [{''.join([item['data'] for item in backup])}]")
-                for i in range(len(backup)):
-                    self.reply_queue.insert(0, backup[-(1 + i)])
-            raise e
-
-    def stream_reply(self, ask_message):
+    def chatgpt_stream_reply(self, ask_message):
         bot = self.bot_manager.get_bot(self.user_id)
         logger.info(f'[线程启动] [{self.user_id}] {ask_message}')
         error = ''
         try:
             for item in bot.ask_stream(ask_message):
-                self.reply_queue.append({'data': item, 'finish': False})
-            self.reply_queue.append({'data': '', 'finish': True})
+                self.reply_cache.append({'data': item, 'finish': False})
+            self.reply_cache.append({'data': '', 'finish': True})
         except chatgpt_api.ContextLengthError as e:
             error = str(e)
             self.bot_manager.clear_bot(self.user_id)
-            self.reply_queue.append({'data': '[抱歉，对话超过模型支持长度，已重置上下文]', 'finish': True})
+            self.reply_cache.append({'data': '[抱歉，对话超过模型支持长度，已重置上下文]', 'finish': True})
         except Exception as e:
             error = str(e)
-            self.reply_queue.append({'data': '[抱歉，连接超时，请重新尝试]', 'finish': True})
+            self.reply_cache.append({'data': '[抱歉，连接超时，请重新尝试]', 'finish': True})
         logger.warning(f'[线程退出] [{self.user_id}] {ask_message} {error}')
-        self.pending = False
+        self.run_task = False
 
 
 @router.websocket('/ws/{user_id}/')
@@ -156,10 +145,10 @@ async def websocket_endpoint(user_id: str, websocket: WebSocket):
     logger.info(f'[WS连接成功] [{user_id}]')
     user = ws_user_manager.get(user_id)
     if not user:
-        user = WsUser(user_id, bot_manager)
+        user: NewUser = NewUser(user_id, bot_manager)
         ws_user_manager[user_id] = user
+    asyncio.create_task(user.reply_task(websocket))
     try:
-        asyncio.create_task(user.send_reply(websocket))
         while True:
             ask = await websocket.receive_text()
             try:
@@ -169,10 +158,9 @@ async def websocket_endpoint(user_id: str, websocket: WebSocket):
                     if json_message['version'] in websocket.app.state.block_version:
                         await websocket.send_json({'data': ask, 'finish': True})
                         continue
-            except:
+            except Exception as e:
                 pass
-            if not user.ask(ask):
+            if not user.ask_task(ask):
                 await websocket.send_json({'data': '请等待上一条消息处理完毕后发送', 'finish': True})
-            await user.send_reply(websocket)
     except WebSocketDisconnect:
         logger.warning(f'[WS断开连接] [{user_id}]')
